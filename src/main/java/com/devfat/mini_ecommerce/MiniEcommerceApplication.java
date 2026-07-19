f8f157bba0a8b5af0f6bf21d56e1c89054b41da8
phathuu26 <huuphat263@gmail.com>
2026-07-19 20:29:02 +0700

    add schedule

--- src/main/java/com/devfat/mini_ecommerce/MiniEcommerceApplication.java
+++ src/main/java/com/devfat/mini_ecommerce/MiniEcommerceApplication.java
@@ -2,9 +2,11 @@ package com.devfat.mini_ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class MiniEcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MiniEcommerceApplication.class, args);

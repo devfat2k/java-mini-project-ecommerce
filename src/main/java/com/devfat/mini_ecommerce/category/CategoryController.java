67a2586451395c392c314aa2b2f558106789151b
phathuu26 <huuphat263@gmail.com>
2026-08-11 23:54:19 +0700

    admin categories

--- src/main/java/com/devfat/mini_ecommerce/category/CategoryController.java
+++ src/main/java/com/devfat/mini_ecommerce/category/CategoryController.java
@@ -1,66 +1,26 @@
 package com.devfat.mini_ecommerce.category;
 
 import com.devfat.mini_ecommerce.category.dto.CategoryResponseDto;
-import com.devfat.mini_ecommerce.category.dto.CreateCategoryRequestDto;
 import com.devfat.mini_ecommerce.shared.base.ApiResponse;
-import com.devfat.mini_ecommerce.shared.base.PageResponse;
-
-
-
-
-
-
-
-
-
-
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/api/v1/categories")
 @AllArgsConstructor
-@Tag(name = "Category", description = "Loại sản phẩm")
+@Tag(name = "Category", description = "Public Category APIs")
 public class CategoryController {
     private final CategoryService categoryService;
 
-    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
-    public ResponseEntity<ApiResponse<CategoryResponseDto>> createCategory(
-            @Valid @RequestBody CreateCategoryRequestDto createCategoryRequestDto) {
-        CategoryResponseDto categoryResponse = categoryService.create(createCategoryRequestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(
-                ApiResponse.success(
-                        categoryResponse,
-                        "Create Category Successfully!"
-                ));
-    }
-
-//    @GetMapping
-//    public ResponseEntity<ApiResponse<PageResponse<CategoryResponseDto>>> getCategories(
-//            @RequestParam(required = false, defaultValue = "") String search,
-//            Pageable pageable) {
-//
-//        Page<CategoryResponseDto> categoryResponse = categoryService.findByNameContainingIgnoreCase(search, pageable);
-//
-//        return ResponseEntity.ok(ApiResponse.success(
-//               PageResponse.of(categoryResponse),
-//                "Get Category Successfully!"
-//        ));
-//    }
-
-     @GetMapping()
-     public ResponseEntity<ApiResponse<List<CategoryResponseDto>>> getAllCategories(){
+    @Operation(summary = "Get active categories", description = "Retrieve a list of all active categories.")
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<CategoryResponseDto>>> getAllCategories(){
         List<CategoryResponseDto> categoryResponse = categoryService.countActiveCategories();
         return ResponseEntity.status(HttpStatus.OK).body(
                 ApiResponse.success(
@@ -68,8 +28,9 @@ public class CategoryController {
                         "Get All Categories Successfully!"
                 )
         );
-     }
+    }
 
+    @Operation(summary = "Get category by ID", description = "Retrieve details of a specific category by ID.")
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<CategoryResponseDto>> getCategoriesById(@PathVariable Long id) {
         return ResponseEntity.ok().body(
@@ -79,29 +40,5 @@ public class CategoryController {
                 )
         );
     }
-
-    @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<CategoryResponseDto>> updateCategory(
-           @PathVariable Long id, @Valid @RequestBody CreateCategoryRequestDto createCategoryRequestDto) {
-        CategoryResponseDto categoryResponse = categoryService.update(id, createCategoryRequestDto);
-        return ResponseEntity.status(HttpStatus.OK).body(
-                ApiResponse.success(
-                        categoryResponse,
-                        "Update Category Successfully!"
-                )
-        );
-    }
-
-
-    @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<Boolean>> deleteCategory(@PathVariable Long id) {
-        boolean isDeletedCategory = categoryService.deleteById(id);
-        return ResponseEntity.ok().body(
-                ApiResponse.success(isDeletedCategory, "Delete Category Successfully!")
-        );
-    }
-
 
 }
